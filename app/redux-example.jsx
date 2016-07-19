860cebb5c34d7067743def7218adeb9bf97b06cd
emileae <Omoikane@Emiles-Mac-mini.bkl38.local>
2016-07-19 14:07:09 +0200

    added redux actions and tested dispatching

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -3,10 +3,26 @@ var redux = require('redux');
 console.log('Starting redux example');
 
 var reducer = (state = {name: 'Anonymous'}, action) => {
-  return state;
-} ;
+  console.log('New action', action);
+  switch (action.type){
+    case 'CHANGE_NAME':
+      return {
+        ...state,
+        name: action.name
+      };
+    default:
+      return state
+  };
+};
 
 var store = redux.createStore(reducer);
 
 var currentState = store.getState();
 console.log('currentState', currentState);
+
+
+store.dispatch({
+  type: "CHANGE_NAME",
+  name: 'Emile'
+});
+console.log("name should be emile", store.getState());
--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -4,14 +4,28 @@ console.log('Starting redux todo example');
 
 var stateDefault = {
   searchText: '',
-  showCompleted: false, 
+  showCompleted: false,
   todos: []
 }
 var reducer = (state = stateDefault, action) => {
-  return state;
+  switch (action.type) {
+    case 'CHANGE_SEARCH_TEXT':
+      return {
+        ...state,
+        searchText: action.searchText
+      }
+    default:
+      return state
+  };
 };
 
 var store = redux.createStore(reducer);
 
 var currentState = store.getState();
 console.log('currentState', currentState);
+
+store.dispatch({
+  type: "CHANGE_SEARCH_TEXT",
+  searchText: "new search"
+});
+console.log('new state afte action', store.getState());
